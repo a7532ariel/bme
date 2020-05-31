4bb6df6470d69c74b2bc5ea1425f6fc60c089408
楊采綸 <lunyang@lun-MacBook-Pro.local>
2020-05-31 23:28:04 +0800

    function : record

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Pause_Img from './assests/pause_button@2x.png'
+import Record_Img from './assests/record_button@2x.png'
+
 import './App.css';
+var MediaStreamRecorder = require('msr');
+
+const apiUrl = "http://140.112.29.224:1234/recognize"
+
+var mediaConstraints = {
+  audio: true
+};
+
+let b = []
+let mediaRecorder;
 
 function App() {
+  const [isRecording, setIsRecording] = useState(false)
+  const [recognizeResult, setRecognizeResult] = useState('')
+
+  function onMediaSuccess(stream) {
+    mediaRecorder = new MediaStreamRecorder(stream);
+    mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+    mediaRecorder.ondataavailable = function (blob) {
+        console.log('chunk of real-time data is: ', blob);
+        // POST/PUT "Blob" using FormData/XHR2
+        b.push(blob);
+    };
+    mediaRecorder.start();
+  }
+
+  function onMediaError () {
+    console.log('onMediaError')
+  }
+
+  const startRecording = () => {
+    setIsRecording(true)
+    setRecognizeResult('')
+    navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+  }
+
+  const stopRecording = () => {
+    mediaRecorder.stop()
+    setIsRecording(false)
+    window.ConcatenateBlobs(b, 'audio/wav', function(concatenatedBlob) {
+      console.log('recordedBlob is: ', concatenatedBlob);
+      let form = new FormData();
+      form.append('file', concatenatedBlob, 'test');
+      console.log(form.get('file'))
+      let requestOptions = {
+        method: 'POST',
+        body: form,
+        // mode:'no-cors',
+      };
+      fetch(apiUrl, requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        console.log(result)
+        setRecognizeResult(result)
+      })
+      .catch(error => console.log('error', error));
+    });
+    b = [];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>
+            {
+              isRecording === false && 
+                <img src={Record_Img} alt="start" onClick={startRecording}/>
+            }
+            {
+              isRecording === true && 
+                <img src={Pause_Img} alt="start" onClick={stopRecording}/>
+            }
+          </div>
+          {
+            recognizeResult !== '' && recognizeResult !== undefined &&
+            <p>
+              result: {recognizeResult}
+            </p>
+          }
+        </div>
       </header>
     </div>
   );
